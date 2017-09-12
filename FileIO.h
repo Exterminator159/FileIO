82c61085a9056db22074726d50444d23a0fea04e
Tochigi <bailey.brownie60@gmail.com>
2017-09-11 22:40:48 -0400

    Update FileIO.h

--- FileIO.h
+++ FileIO.h
@@ -1,12 +1,5 @@
 #include <ntddk.h>
 
-/*
-	Example:
-	FileStruct WriteStatus = WriteFile("C:\\test.txt", "WriteFile!", TRUE);
-	FileStruct ReadStatus = ReadFile("C:\\test.txt");
-	DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "%s\n", ReadStatus.Value);
-*/
-
 typedef struct _FileStruct
 {
 	NTSTATUS Status;
